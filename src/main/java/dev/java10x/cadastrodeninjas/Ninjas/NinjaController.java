f7446437f4c95d21afb9caff67d316573cbef9fb
Artur <artur.metallica3000@gmail.com>
2026-01-02 13:50:53 -0300

    feat: Adding endpoints for the class NinjaControler

--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.java10x.cadastrodeninjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,33 @@ public class NinjaController {
     @GetMapping("/boasvindas")// Pega as informações, nesse caso ele vai pegar o método boasVindas
     public String boasVindas() {
         return "Essa é minha primeira mensagem nessa rota";
+    }
+
+    //TODO: Adicionar ninja (Create)
+    @PostMapping("/create")
+    public String criarNinja() {
+        return "Ninja Criado";
+    }
+    //TODO: Mostrar ninja por ID (Update)
+    @GetMapping("/ninjaid")
+    public  String MostrarNinjaPorId() {
+        return "Mostra um ninja por ID";
+    }
+
+    //TODO Mostrar todos os ninjas (Update)
+    @GetMapping("/all")
+    public  String MostrarTodosOsNinjas() {
+        return "Todos os ninjas";
+    }
+    //TODO: Alterar dados do Ninja (Update)
+    @PutMapping("/update")
+    public String AlterarONinjaPorID() {
+        return "Ninja Alterado";
+    }
+
+    //TODO: Deletar Ninja (Delete)
+    @DeleteMapping("/delete")
+    public  String DeletarPorId() {
+        return "Ninja deletado";
     }
 }
