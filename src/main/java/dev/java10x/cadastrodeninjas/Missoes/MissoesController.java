8316f7204a32691ef19382ed94b15d9f4a48d468
Artur <artur.metallica3000@gmail.com>
2026-01-02 14:06:00 -0300

    feat: adding endPoints in the class missoes

--- src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesController.java
@@ -1,4 +1,34 @@
 package dev.java10x.cadastrodeninjas.Missoes;
 
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
 public class MissoesController {
+
+    @PostMapping("/missoes/criar")
+    public String CreateMissoes() {
+        return "Missoes criadas";
+    }
+
+    @GetMapping("/missoes/mostrarone")
+    public String ProcuraPorId() {
+        return "Missao retornada";
+    }
+
+    @GetMapping("/missoes/mostrarall")
+    public  String MostrarTodasAsMissoes() {
+        return "Mostrando todas as Missoes";
+    }
+
+    @DeleteMapping("/missoes/deletar")
+    public String DeletaUmaMissao() {
+        return "Missao deletada";
+    }
+
+    @PutMapping("/missoes/update")
+    public  String UpdateMissao() {
+        return "Missao atualizada";
+    }
 }
