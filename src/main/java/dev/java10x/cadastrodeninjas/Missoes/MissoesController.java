f22e645ee351daa8a09397be188ac4785ab23cb3
Artur <artur.metallica3000@gmail.com>
2026-01-05 09:01:56 -0300

    feat: adding findbyId and findAll in missoesService and adding route in MissoesController

--- src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesController.java
@@ -1,25 +1,34 @@
 package dev.java10x.cadastrodeninjas.Missoes;
 
+import dev.java10x.cadastrodeninjas.Ninjas.NinjaModel;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/missoes")
 public class MissoesController {
+
+    private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
 
     @PostMapping("/criar")
     public String CreateMissoes() {
         return "Missoes criadas";
     }
 
-    @GetMapping("/mostrarone")
-    public String ProcuraPorId() {
-        return "Missao retornada";
+    @GetMapping("/listar/{id}")
+    public MissoesModel ProcuraPorId(@PathVariable Long id) {
+        return missoesService.listarMissaoPorId(id);
     }
 
-    @GetMapping("/mostrarall")
-    public  String MostrarTodasAsMissoes() {
-        return "Mostrando todas as Missoes";
+    @GetMapping("/listar")
+    public List<MissoesModel> MostrarTodasAsMissoes() {
+        return missoesService.listarMissoes();
     }
 
     @DeleteMapping("/deletar")
--- src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesService.java
@@ -1,4 +1,25 @@
 package dev.java10x.cadastrodeninjas.Missoes;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MissoesService {
+
+    private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    public List<MissoesModel> listarMissoes() {
+        return missoesRepository.findAll();
+    }
+
+    public MissoesModel listarMissaoPorId(Long id) {
+        Optional<MissoesModel> missaoporId = missoesRepository.findById(id);
+        return  missaoporId.orElse(null);
+    }
 }
