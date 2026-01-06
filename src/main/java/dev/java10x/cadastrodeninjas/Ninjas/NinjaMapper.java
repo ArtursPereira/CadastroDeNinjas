2b7b0c08e61373c24649287517a5e3784ab8e4fc
Artur <artur.metallica3000@gmail.com>
2026-01-06 09:36:16 -0300

    chore: It includes DTOs as an extra layer of security to make it more difficult for the user to access the database.

--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaMapper.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,34 @@
 package dev.java10x.cadastrodeninjas.Ninjas;
 
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public static NinjaModel map(NinjaDTO ninjaDTO) {
+        NinjaModel ninjaModel = new NinjaModel();
+
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setRank(ninjaDTO.getRank());
+
+        return ninjaModel;
+    }
+
+    public static NinjaDTO map(NinjaModel ninjaModel) {
+        NinjaDTO ninjaDTO = new NinjaDTO();
+
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setRank(ninjaModel.getRank());
+
+        return ninjaDTO;
+    }
 }
