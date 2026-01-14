4628d39b4bde7d2b38a5ccbd5031145cb8836768
Artur <artur.metallica3000@gmail.com>
2026-01-14 13:13:04 -0300

    feat: adding annotation mapper in MissoesMapper and switch the methods

--- src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesMapper.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesMapper.java
@@ -2,28 +2,14 @@ package dev.java10x.cadastrodeninjas.Missoes;
 
 
 import dev.java10x.cadastrodeninjas.Ninjas.NinjaDTO;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Component
-public class MissoesMapper {
 
-    public static MissoesModel map(MissoesDTO missoesDTO) {
-        MissoesModel missoesModel = new MissoesModel();
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface MissoesMapper {
 
-        missoesModel.setId(missoesDTO.getId());
-        missoesModel.setDificuldade(missoesDTO.getDificuldade());
-        missoesModel.setNome(missoesDTO.getNome());
-        missoesModel.setNinjas(missoesDTO.getNinjas());
-        return missoesModel;
-    }
+    MissoesDTO map(MissoesModel missoesModel);
 
-    public static MissoesDTO map(MissoesModel missoesModel) {
-        MissoesDTO missoesDTO = new MissoesDTO();
-
-        missoesDTO.setId(missoesModel.getId());
-        missoesDTO.setNome(missoesModel.getNome());
-        missoesDTO.setDificuldade(missoesModel.getDificuldade());
-        missoesDTO.setNinjas(missoesModel.getNinjas());
-        return  missoesDTO;
-    }
+    MissoesModel map(MissoesDTO missoesDTO);
 }
